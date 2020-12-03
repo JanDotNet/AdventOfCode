0f635ab69a743af84f303936ef9eb8b06e6ca4b7
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-03 21:24:46 +0100

    added day 2020.3

---
+++ AdventOfCode2020/AdventOfCode2020/Day03_1.fsx
@@ -0,0 +1,14 @@
+﻿open System.IO
+open System
+open System.Text.RegularExpressions
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day03d.txt")
+
+module Seq = let rec cycle xs = seq { yield! xs; yield! cycle xs }
+
+let result = File.ReadAllLines(file)
+            |> Seq.map Seq.cycle
+            |> Seq.indexed
+            |> Seq.map (fun (i, row) -> row |> Seq.item (i * 3))
+            |> Seq.filter (fun x -> x = '#')
+            |> Seq.length
---
+++ AdventOfCode2020/AdventOfCode2020/Day03_2.fsx
@@ -0,0 +1,29 @@
+﻿open System.IO
+open System
+open System.Text.RegularExpressions
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day03.txt")
+
+module Seq = let rec cycle xs = seq { yield! xs; yield! cycle xs }
+
+let lines = File.ReadAllLines(file)
+
+let xRightOneDown lst x = lst |> Seq.map Seq.cycle
+                              |> Seq.indexed
+                              |> Seq.map (fun (i, row) -> row |> Seq.item (i * x))
+                              |> Seq.filter (fun x -> x = '#')
+                              |> Seq.length
+
+let OneRightxDown lst x = lst |> Seq.map Seq.cycle
+                              |> Seq.indexed
+                              |> Seq.filter (fun (i, row) -> i <> 0 && i % 2 = 0)
+                              |> Seq.map (snd)
+                              |> Seq.indexed
+                              |> Seq.map (fun (i, row) -> row |> Seq.item (i + 1))
+                              |> Seq.filter (fun x -> x = '#')
+                              |> Seq.length
+
+let results = ([1; 3; 5; 7] |> Seq.map (xRightOneDown lines) |> Seq.toList) @
+              ([2] |> Seq.map (OneRightxDown lines) |> Seq.toList)
+
+let result = results |> List.map int64 |> List.reduce (*)
