918517bb364b93ff78292ccaa2a90cba36b15462
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-05 15:08:48 +0100

    cleanup

--- AdventOfCode2020/AdventOfCode2020/Day05_2.fsx
+++ AdventOfCode2020/AdventOfCode2020/Day05_2.fsx
@@ -3,24 +3,27 @@ open System.IO
 
 let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day05.txt")
 
-let codeToList s l (code:string) = code.Substring(s, l) |> Seq.rev |> Seq.toList
-let splitRowAndColCodes code = code |> codeToList 0 7, code|> codeToList 7 3
-let pow2 (x:int) = Math.Pow(2.0, x |> float) |> int
+let getSubCode idx len (code:string) = code.Substring(idx, len) |> Seq.rev |> Seq.toList
+let splitRowAndColCodes code = code |> getSubCode 0 7, code|> getSubCode 7 3
 
 let rec decode bitChar (num, idx) = function
   | []      -> num
-  | h :: t  -> let numNew =  num + (if h = bitChar then pow2 idx else 0)
+  | h :: t  -> let numNew =  num + (if h = bitChar then pown 2 idx else 0)
                t |> (decode bitChar (numNew, idx+1))
 
 let decodeRowCode = decode 'B' (0, 0)
 let decodeColCode = decode 'R' (0, 0)
+let decodeRowAndColCode (rowCode, colCode) = decodeRowCode rowCode, decodeColCode colCode
+let codeToSeatId (row, col) = row * 8 + col
+let areNumbersNotAdjacent(n1:int, n2:int) = abs (n1-n2) <> 1
+let getNumberBeween (n1, n2) = (n1 + n2) / 2
 
 let result = File.ReadAllLines(file)
              |> Seq.map splitRowAndColCodes             
-             |> Seq.map (fun (row, col) -> decodeRowCode row, decodeColCode col)            
-             |> Seq.map (fun (row, col) -> row * 8 + col)
+             |> Seq.map decodeRowAndColCode
+             |> Seq.map codeToSeatId
              |> Seq.sort
              |> Seq.pairwise
-             |> Seq.filter (fun (prev, cur) -> prev <> (cur - 1) && cur <> (prev - 1))
-             |> Seq.map (fun (prev, cur) -> (prev + cur) / 2)
+             |> Seq.filter areNumbersNotAdjacent
+             |> Seq.map getNumberBeween
              |> Seq.exactlyOne
