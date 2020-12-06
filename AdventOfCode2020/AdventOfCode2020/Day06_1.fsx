55ee91be6a69adb2c473977de0b5b8bfc55cf0ea
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-06 09:51:18 +0100

    added day 6

---
+++ AdventOfCode2020/AdventOfCode2020/Day06_1.fsx
@@ -0,0 +1,15 @@
+﻿open System.IO
+open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day06.txt")
+
+let foldGroups (agg, lst) = function
+                            | "" -> ( [], agg :: lst)
+                            | item -> ( (item |> Seq.toList) :: agg), lst
+
+let allGroups = File.ReadAllLines(file) |> Seq.fold foldGroups ([], []) |> snd
+
+let uniqueAnswersPerGroup group = 
+    group |> (List.concat >> List.distinct >> List.length)
+
+let result = allGroups |> List.map uniqueAnswersPerGroup |> List.sum
---
+++ AdventOfCode2020/AdventOfCode2020/Day06_2.fsx
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day06.txt")
+
+let foldGroups (agg, lst) = function
+                            | "" -> ( [], agg :: lst)
+                            | item -> ( (item |> Seq.toList) :: agg), lst
+
+let allGroups = File.ReadAllLines(file) |> Seq.fold foldGroups ([], []) |> snd
+let uniqueQuestions = allGroups |> (List.concat >> List.concat >> List.distinct)
+
+let hasPersonAnsweredQuestion question person = 
+    person |> List.contains question
+
+let haveAllPersonsAnswerdQuestion group question = 
+    group |> List.forall (hasPersonAnsweredQuestion question)
+
+let countQuestionsAnsweredFromAll group = 
+    uniqueQuestions |> List.filter (haveAllPersonsAnswerdQuestion group) |> List.length
+
+let result = allGroups |> List.map countQuestionsAnsweredFromAll |> List.sum
