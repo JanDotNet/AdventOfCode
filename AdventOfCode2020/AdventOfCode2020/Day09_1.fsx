12892516fe6573c16e0386d80ad9a08b82bfefd7
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-09 09:16:40 +0100

    added day 9

---
+++ AdventOfCode2020/AdventOfCode2020/Day09_1.fsx
@@ -0,0 +1,14 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day09.txt")
+let numbers = File.ReadAllLines(file) |> Array.toList |> List.map int64
+
+let preambleSize = 25
+
+let sumPairs l 
+    = (List.allPairs l l) |> List.map (fun (x, y) -> x + y) |> List.distinct
+
+let preambles = numbers |> List.take (numbers.Length - 1) |> List.windowed preambleSize |> List.map sumPairs
+let numbersToCheck = numbers |> List.skip preambleSize
+let preambleNumberPairs = List.zip numbersToCheck preambles
+let result = preambleNumberPairs |> List.filter (fun (num, lst) -> lst |> List.contains num |> not) |> List.map fst
---
+++ AdventOfCode2020/AdventOfCode2020/Day09_2.fsx
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day09.txt")
+
+let allNumbers = File.ReadAllLines(file) |> Array.map int64
+let target = 10884537L
+
+let findContiguousRange num (numbers:int64 array) =
+    let rec findContiguousRange' (idx, length) =
+        let range = numbers.[idx..length]
+        let sum = range |> Array.sum
+        if sum = num then
+            range
+        elif sum < num then 
+            findContiguousRange' (idx, length + 1)
+        else 
+            findContiguousRange' (idx + 1, 2)
+    findContiguousRange' (0, 2)
+
+let contiguousRange = allNumbers |> findContiguousRange target
+let result = (contiguousRange |> Array.min) + (contiguousRange |> Array.max)
