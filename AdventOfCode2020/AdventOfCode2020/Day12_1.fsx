b358edb2022211fe74bb9e65630ba515897e108b
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-12 11:00:05 +0100

    added day 12

---
+++ AdventOfCode2020/AdventOfCode2020/Day12_1.fsx
@@ -0,0 +1,40 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day12.txt")
+let input = File.ReadAllLines(file) |> Array.toList
+
+(* Types *)
+type State = { Dir:char; N:int; E:int; S:int; W:int } with 
+  static member Initial = { Dir = 'E'; N = 0; E = 0; S = 0; W = 0}
+
+(* Parsing *)
+let parseLine (line:string) = (line |> Seq.head, line.Substring(1) |> int)
+  
+(* State Change *)
+let run instructions =
+    let turnLeft = [('N', 'W'); ('W', 'S'); ('S', 'E'); ('E', 'N')]
+    let turnRight = [('N', 'E'); ('E', 'S'); ('S', 'W'); ('W', 'N')]
+    let turnAround = [('N', 'S'); ('W', 'E'); ('E', 'W'); ('S', 'N')]    
+    let apply turns d = 
+      turns |> List.find (fun (s, _) -> s = d) |> snd
+
+    let rec processInstruction state = function
+      | ('N', num) -> { state with N = state.N + num }
+      | ('E', num) -> { state with E = state.E + num }
+      | ('S', num) -> { state with S = state.S + num }
+      | ('W', num) -> { state with W = state.W + num }
+      | ('L',  90) 
+      | ('R', 270) -> { state with Dir = state.Dir |> apply turnLeft }
+      | ('R',  90) 
+      | ('L', 270) -> { state with Dir = state.Dir |> apply turnRight }
+      | ('L', 180) 
+      | ('R', 180) -> { state with Dir = state.Dir |> apply turnAround }
+      | ('F', num) -> processInstruction state (state.Dir, num)
+      | (c, n)-> failwith (sprintf "Should not happen %c %i" c n)
+
+    instructions |> List.fold processInstruction State.Initial
+
+let getDistance (s:State) = abs (s.N - s.S) + abs (s.W - s.E)
+
+let result = input |> List.map parseLine |> run |> getDistance
+    
---
+++ AdventOfCode2020/AdventOfCode2020/Day12_2.fsx
@@ -0,0 +1,38 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day12.txt")
+let input = File.ReadAllLines(file) |> Array.toList
+
+(* Types *)
+type State = { ShipX: int; ShipY: int; WaypointX: int; WaypointY: int } with 
+  static member Initial = { ShipX = 0; ShipY = 0; WaypointX = 10; WaypointY = -1}
+
+(* Parsing *)
+let parseLine (line:string) = (line |> Seq.head, line.Substring(1) |> int)
+  
+(* State Change *)
+let run instructions =
+    let turnLeft s =   { s with WaypointX =  s.WaypointY; WaypointY = -s.WaypointX }
+    let turnRight s =  { s with WaypointX = -s.WaypointY; WaypointY = s.WaypointX }
+    let turnAround s = { s with WaypointX = -s.WaypointX; WaypointY = -s.WaypointY }   
+
+    let rec processInstruction (s:State) = function
+      | ('N', num) -> { s with WaypointY = s.WaypointY - num  }
+      | ('E', num) -> { s with WaypointX = s.WaypointX + num  }
+      | ('S', num) -> { s with WaypointY = s.WaypointY + num  }
+      | ('W', num) -> { s with WaypointX = s.WaypointX - num  }
+      | ('L',  90) 
+      | ('R', 270) -> s |> turnLeft
+      | ('R',  90) 
+      | ('L', 270) -> s |> turnRight
+      | ('L', 180) 
+      | ('R', 180) -> s |> turnAround
+      | ('F', num) -> { s with ShipX = s.ShipX + num * s.WaypointX; ShipY = s.ShipY + num * s.WaypointY }
+      | (c, n)-> failwith (sprintf "Should not happen %c %i" c n)
+
+    instructions |> List.fold processInstruction State.Initial
+
+let getDistance (s:State) = abs (s.ShipY) + abs (s.ShipX)
+
+let result = input |> List.map parseLine |> run |> getDistance
+    
