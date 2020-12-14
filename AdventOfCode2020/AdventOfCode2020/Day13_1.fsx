de8ffe2a97e34aa2d7892411ff09bb2a98431b9d
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-14 22:17:31 +0100

    added day 13 (WIP)

---
+++ AdventOfCode2020/AdventOfCode2020/Day13_1.fsx
@@ -0,0 +1,17 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day13.txt")
+let lines = File.ReadAllLines(file)
+ 
+let timestamp = lines.[0] |> int
+let busIds = lines.[1].Split(',') |> Array.toList |> List.filter (fun e -> e <> "x") |> List.map int
+
+let nextTimestamp timestamp busId =
+  let nextTimeStampFactor = ((timestamp |> float) / (busId |> float) |> int) + 1
+  nextTimeStampFactor * busId
+  
+let nextBus = busIds 
+              |> List.map (fun id -> (id, id |> nextTimestamp timestamp)) 
+              |> List.minBy snd
+
+let result = (nextBus |> fst) * ((nextBus |> snd) - timestamp) 
---
+++ AdventOfCode2020/AdventOfCode2020/Day13_2.fsx
@@ -0,0 +1,30 @@
+﻿open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day13.txt")
+
+// WIP
+//let line = File.ReadAllLines(file)
+//              |> Array.skip 1
+//              |> Array.exactlyOne
+
+let line = "7,13,x,x,59,x,31,19"
+let split (l:string) = l.Split(',')
+                    |> Array.toList
+                    |> List.mapi (fun idx el -> (idx, el))
+                    |> List.filter (fun (_, el) -> el <> "x") 
+                    |> List.map (fun (idx, el) -> (el |> int |> bigint ), idx |> bigint )
+                    |> List.sortBy snd
+                    |> List.rev
+
+let busIds = split line
+ 
+let busId_0 = busIds |> List.head |> snd 
+let busIds_rem = busIds.Tail
+
+let rec solve timestamp =
+  //printfn "%s" timestamp.ToString()
+  let found = busIds_rem |> List.forall (fun (num, id) -> (bigint.Add (timestamp, num)) % (id) = bigint.Zero)
+  if found then timestamp
+  else solve (timestamp + busId_0)
+
+solve busId_0
