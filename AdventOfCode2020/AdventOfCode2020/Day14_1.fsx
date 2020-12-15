5f7b0fe153d9666a143b45a75dda62b2aefdba40
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-15 09:02:54 +0100

    added day 14

---
+++ AdventOfCode2020/AdventOfCode2020/Day14_1.fsx
@@ -0,0 +1,55 @@
+﻿open System
+open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day14.txt")
+let lines = File.ReadAllLines(file) |> Array.toList
+ 
+(* Types *)
+type Program<'a> = { Mask:string; Mem:Map<int, 'a> }
+  with static member Empty = { Mask = ""; Mem = Map.empty}
+
+(* Helper *)
+let rec decToBin dec =
+  match dec with
+  | 0 | 1 -> string dec
+  | _ ->
+      let bit = string (dec % 2)
+      (decToBin (dec / 2)) + bit
+
+let rec binToDec dec bin =
+  match bin with
+  | [] -> dec
+  | '0'::t -> binToDec dec t
+  | '1'::t -> binToDec (bigint.Add(dec, (bigint.Pow(2I, t.Length)))) t
+  | x::t -> failwithf "Invalid char in binary string: %c" x
+
+let applyMask mask value =
+  (value |> Seq.zip mask) |> Seq.map (fun (m, v) -> if m <> 'X' then m else v) |> Seq.toList
+
+let run prog =
+  { Mask = prog.Mask; Mem = prog.Mem |> Map.map (fun k v -> (v |> applyMask prog.Mask) |> binToDec bigint.Zero) }
+
+(* parsing *)
+let parseLines lines = 
+  let fold (current, progs) (line:string) =
+    if line.StartsWith("mask = ") then
+      ( { Mask = line.Substring(7); Mem = Map.empty }, if current = Program<int>.Empty then progs else current :: progs )
+    else
+      let splitted = line.Split([|"mem["; "] = "|], StringSplitOptions.RemoveEmptyEntries)
+      let pos = splitted.[0] |> int
+      let value = (splitted.[1] |> int |> decToBin).PadLeft(36, '0')
+      ( { current with Mem = current.Mem |> Map.add pos value },  progs)
+  let parsed = lines |> List.fold fold (Program<int>.Empty, [])
+  (fst parsed) :: (snd parsed)
+
+(* final computation *)
+let result = lines 
+              |> parseLines 
+              |> List.map run 
+              |> List.map (fun p -> p.Mem |> Map.toList)
+              |> List.collect id
+              |> List.rev
+              |> Map.ofList
+              |> Map.toList
+              |> List.map snd
+              |> List.sum
---
+++ AdventOfCode2020/AdventOfCode2020/Day14_2.fsx
@@ -0,0 +1,62 @@
+﻿open System
+open System.IO
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day14.txt")
+let lines = File.ReadAllLines(file) |> Array.toList
+ 
+(* Helper *)
+let padLeft n c (s:string) =
+  s.PadLeft(n, c)
+
+let rec decToBin dec =
+  match dec with
+  | 0 | 1 -> string dec
+  | _ ->
+      let bit = string (dec % 2)
+      (decToBin (dec / 2)) + bit
+
+let rec binToDec dec bin =
+  match bin with
+  | [] -> dec
+  | '0'::t -> binToDec dec t
+  | '1'::t -> binToDec (bigint.Add(dec, (bigint.Pow(2I, t.Length)))) t
+  | x::t -> failwithf "Invalid char in binary string: %c" x
+
+let generatFloatings n 
+    = [0..(pown 2 n)-1] |> List.map (fun x -> x |> decToBin |> padLeft n '0' |> Seq.map id |> Seq.toList)
+
+let applyMask mask value =
+    let masked = (value |> Seq.zip mask) |> Seq.map (fun (m, v) -> if m <> '0' then m else v) |> Seq.toList
+    let numOfFloatingBits = masked |> Seq.filter (fun x -> x = 'X') |> Seq.length
+    let floatingVariations = generatFloatings numOfFloatingBits |> Seq.toList
+    let folder (res, f:char list) = function
+                                    | 'X' -> (f.Head :: res, f.Tail)
+                                    | x   -> (x :: res, f)
+    floatingVariations |> List.map (fun float -> masked |> List.fold folder ([], float) |> fst |> List.rev)
+
+let run (mask, pos, (value:int)) =
+  let binPositions = pos |> decToBin |> padLeft 36 '0' |> applyMask mask
+  binPositions |> List.map (fun bPos -> (bPos |> binToDec 0I, bigint(value)))
+
+(* parsing *)
+let parseLines lines = 
+  let fold (mask, progs) (line:string) =
+    if line.StartsWith("mask = ") then
+      ( line.Substring(7), progs )
+    else
+      let splitted = line.Split([|"mem["; "] = "|], StringSplitOptions.RemoveEmptyEntries)
+      let pos = splitted.[0] |> int
+      let value = (splitted.[1] |> int)
+      ( mask, (mask, pos, value) :: progs)
+  lines |> List.fold fold ("", []) |> snd
+
+(* final computation *)
+let result = lines 
+              |> parseLines 
+              |> List.map run 
+              |> List.collect id
+              |> List.rev
+              |> Map.ofList
+              |> Map.toList
+              |> List.map snd
+              |> List.sum
