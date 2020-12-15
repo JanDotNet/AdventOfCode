43c61f5e19bd22c87c4404bac7b636b4b5d9c64b
Jan-Niklas <JanDotNet@ThinkSharp.de>
2020-12-15 22:34:08 +0100

    added day 15

---
+++ AdventOfCode2020/AdventOfCode2020/Day15_1.fsx
@@ -0,0 +1,16 @@
+﻿let input = "8,13,1,0,18,9"
+let initialNumbers = input.Split(',') |> Array.toList |> List.map int |> List.rev
+
+let initialRow = initialNumbers.Length
+let initialItem = initialNumbers.Head
+let initialItems = initialNumbers.Tail |> List.rev
+let initialMap = initialItems |> List.mapi (fun idx item -> (item, idx + 1)) |> Map.ofList
+
+let rec solve (map, lastItem) idx =
+  let nextItem =  match map |> Map.tryFind lastItem with
+                    | Some (number) ->  idx - number
+                    | None -> 0
+  let nextMap = map |> Map.add lastItem idx
+  (nextMap, nextItem)
+
+let result = [initialRow..(2020-1)] |> List.fold solve (initialMap, initialItem)
---
+++ AdventOfCode2020/AdventOfCode2020/Day15_2.fsx
@@ -0,0 +1,19 @@
+﻿open System.Collections.Generic
+
+let input = "8,13,1,0,18,9"
+let initialNumbers = input.Split(',') |> Array.toList |> List.map (int >> int64) |> List.rev
+
+let initialRow = int64(initialNumbers.Length)
+let initialItem = initialNumbers.Head
+
+let initialDict = new Dictionary<int64,int64>()
+for (key, value) in initialNumbers.Tail |> List.rev |> List.mapi (fun idx item -> (item, int64(idx + 1))) do
+  initialDict.Add(key, value)
+
+let rec solve (dict:Dictionary<int64, int64>, lastItem) idx =
+    let found, number = dict.TryGetValue lastItem
+    let nextItem =  if found then idx - number else 0L
+    dict.[lastItem] <- idx
+    (dict, nextItem)
+
+let result = [initialRow..(30000000L-1L)] |> List.fold solve (initialDict, initialItem)
