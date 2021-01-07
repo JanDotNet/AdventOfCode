efad4285f89801ed47458dc9378ec8901bb5529f
Jan-Niklas <JanDotNet@ThinkSharp.de>
2021-01-07 17:56:21 +0100

    added day 13

--- AdventOfCode2020/AdventOfCode2020/Day13_2.fsx
+++ AdventOfCode2020/AdventOfCode2020/Day13_2.fsx
@@ -2,29 +2,29 @@
 
 let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day13.txt")
 
-// WIP
-//let line = File.ReadAllLines(file)
-//              |> Array.skip 1
-//              |> Array.exactlyOne
+let line = File.ReadAllLines(file)
+             |> Array.skip 1
+             |> Array.exactlyOne
 
-let line = "7,13,x,x,59,x,31,19"
-let split (l:string) = l.Split(',')
+let parse (l:string) = l.Split(',')
                     |> Array.toList
                     |> List.mapi (fun idx el -> (idx, el))
                     |> List.filter (fun (_, el) -> el <> "x") 
-                    |> List.map (fun (idx, el) -> (el |> int |> bigint ), idx |> bigint )
+                    |> List.map (fun (idx, el) -> (el |> int |> bigint ),  ((el |> int)-idx) |> bigint )
                     |> List.sortBy snd
-                    |> List.rev
 
-let busIds = split line
- 
-let busId_0 = busIds |> List.head |> snd 
-let busIds_rem = busIds.Tail
+let calcXi ni Ni =
+  Seq.initInfinite (fun i -> bigint(i + 1))
+  |> Seq.map (fun x -> Ni * x % ni, x)
+  |> Seq.find (fun (rem, x) -> rem = bigint(1))
+  |> snd
 
-let rec solve timestamp =
-  //printfn "%s" timestamp.ToString()
-  let found = busIds_rem |> List.forall (fun (num, id) -> (bigint.Add (timestamp, num)) % (id) = bigint.Zero)
-  if found then timestamp
-  else solve (timestamp + busId_0)
-
-solve busId_0
+let busIds = parse line
+let N = busIds |> List.map fst |> List.reduce (*)
+let result = busIds 
+             |> List.map (fun (ni, bi) -> (ni, bi, N / ni))
+             |> List.map (fun (ni, bi, Ni) -> (bi, Ni, calcXi ni Ni))
+             |> List.map (fun (bi, Ni, xi) -> bi * Ni * xi)
+             |> List.sum
+              
+let result' = result % N
