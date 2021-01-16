530c9ee1436e63523401ba93bf4143a21cb21717
Jan-Niklas Schäfer <Jan-NiklasSchaefer@gmx.de>
2021-01-16 23:24:21 +0100

    improved solution

--- AdventOfCode2020/Python/Day06.py
+++ AdventOfCode2020/Python/Day06.py
@@ -5,33 +5,19 @@ with open("Data\Day06.txt") as file:
     block = file.read().split('\n\n')    
     input = list(map(lambda x: x.split('\n'), block))
 
-def solve_part1(data):
+def count(agg, initial, data):
     count = 0
     for answers in data:
-        # functional solution
         s1 = map(lambda x: set(x), answers)
-        s2 = ft.reduce(lambda a, b: a | b, s1, set())
+        s2 = ft.reduce(agg, s1, initial)
         count += len(s2)
-        # Iterative solution
-        # unique = set()
-        # for person_answer in answers:
-        #     unique = unique | set(person_answer)
-        # count += len(unique) 
-    return count    
+    return count
+
+def solve_part1(data):
+    return count(lambda a, b: a | b, set(), data)
     
 def solve_part2(data):
-    count = 0
-    for answers in data:
-        # functional solution
-        s1 = map(lambda x: set(x), answers)
-        s2 = ft.reduce(lambda a, b: a & b, s1, set(s.ascii_lowercase))
-        count += len(s2)
-        # Iterative solution
-        # unique = set(s.ascii_lowercase)
-        # for person_answer in answers:
-        #     unique = unique & set(person_answer)
-        #count += len(unique) 
-    return count
+    return count(lambda a, b: a & b, set(s.ascii_lowercase), data)
 
 print(f"Part 1: {solve_part1(input)}")
 print(f"Part 2: {solve_part2(input)}")
