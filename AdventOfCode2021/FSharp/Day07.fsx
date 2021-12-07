cc089686f3a4fb428466dd82eb26cfe6339c3f80
Jan-Niklas <JanDotNet@ThinkSharp.de>
2021-12-07 07:57:34 +0100

    added day 7

---
+++ AdventOfCode2021/FSharp/Day07.fsx
@@ -0,0 +1,23 @@
+﻿open System.IO
+open System
+
+let file = Path.Combine(__SOURCE_DIRECTORY__, "Data", "Day07.txt")
+
+//let input = "16,1,2,0,4,2,7,1,2,14"
+
+let input = File.ReadAllLines(file) |> Array.head
+
+let positions = input.Split(',') |> Seq.map int64 |> Seq.toList
+
+let get_fuel_consumption crap_positions map target_pos = 
+    let fuels_cost pos = (pos - target_pos) |> abs |> map
+    crap_positions |> List.map fuels_cost |> List.sum
+
+let max_pos = positions |> List.max
+let min_pos = positions |> List.min
+let target_positions = [min_pos .. max_pos]
+
+let solve map = (target_positions |> List.map (get_fuel_consumption positions map) |> List.min)
+
+printfn "Part1: %i" (solve id)
+printfn "Part2: %i" (solve (fun n -> [1L..n] |> List.sum))
